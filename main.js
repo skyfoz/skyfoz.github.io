b578bff07ab59d0bf366ff9cd94bf7aa7e9f3751
skyfoz <s.gourves46@gmail.com>
2025-07-11 01:58:01 +0200

    nsm ya eu un effort

--- main.js
+++ main.js
@@ -11,24 +11,31 @@ function btnMenu() {
     btnClicked = true;
     document.getElementsByClassName("menu")[0].style.backgroundColor = 'rgb(34, 34, 34)';
     document.getElementsByClassName("menu")[0].style.filter = "drop-shadow(0 0 0.2rem rgb(44, 44, 44))";
-    document.getElementsByClassName("side")[0].style.display = "block";
+    document.getElementsByClassName("side")[0].style.left = "0px";
   }
   else
   {
     btnClicked = false;
     document.getElementsByClassName("menu")[0].style.backgroundColor = 'rgb(26, 26, 26)';
     document.getElementsByClassName("menu")[0].style.filter = "drop-shadow(0 0 0.2rem rgb(26, 26, 26)";
-    document.getElementsByClassName("side")[0].style.display = "none";
+    document.getElementsByClassName("side")[0].style.left = "-110%";
   }
 }
 
 function forTestingOrUrWeird(mobileQuery) {
   if (!mobileQuery.matches) {
+    document.getElementsByClassName("side")[0].style.transitionDuration = "0s";
+    setTimeout(function(){
     document.getElementsByClassName("side")[0].style.display = "block";
     document.getElementsByClassName("menu")[0].style.backgroundColor = 'rgb(26, 26, 26)';
     document.getElementsByClassName("menu")[0].style.filter = "drop-shadow(0 0 0.2rem rgb(26, 26, 26)";
+    }, 1);
+    document.getElementsByClassName("side")[0].style.left = "0px";
   } else {
-    document.getElementsByClassName("side")[0].style.display = "none";
+    document.getElementsByClassName("side")[0].style.left = "-110%";
+    setTimeout(function(){
+    document.getElementsByClassName("side")[0].style.transitionDuration = "0.5s";
+    }, 1);
   }
 }
 
