02f62b0a88615c9b9ba9f9be9027b5b3c2f04e01
skyfoz <s.gourves46@gmail.com>
2025-07-10 02:58:27 +0200

    it's not supposed to be useful but for me it is

--- main.js
+++ main.js
@@ -21,3 +21,21 @@ function btnMenu() {
     document.getElementsByClassName("side")[0].style.display = "none";
   }
 }
+
+function forTestingOrUrWeird(mobileQuery) {
+  if (!mobileQuery.matches) {
+    document.getElementsByClassName("side")[0].style.display = "block";
+    document.getElementsByClassName("menu")[0].style.backgroundColor = 'rgb(26, 26, 26)';
+    document.getElementsByClassName("menu")[0].style.filter = "drop-shadow(0 0 0.2rem rgb(26, 26, 26)";
+  } else {
+    document.getElementsByClassName("side")[0].style.display = "none";
+  }
+}
+
+var mobileQuery = window.matchMedia("screen and (max-width: 780px)")
+
+forTestingOrUrWeird(mobileQuery);
+
+mobileQuery.addEventListener("change", function() {
+  forTestingOrUrWeird(mobileQuery);
+});
