e9d941655538110508ef49555f4168d953529507
Skyfoz <s.gourves46@gmail.com>
2025-09-07 17:26:30 +0200

    fsd

--- art/image/image.js
+++ art/image/image.js
@@ -4,3 +4,6 @@ const imageUrl = decodeURIComponent(params.get("image"));
 const para = document.createElement("img");
 para.setAttribute("src", imageUrl);
 para.setAttribute("id", "pageImage");
+
+var main = document.getElementsByTagName("main")[0];
+main.appendChild(para);
