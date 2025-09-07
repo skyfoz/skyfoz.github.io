23bb009d52bae1f70532749ff2e98c6e775adac8
Skyfoz <s.gourves46@gmail.com>
2025-09-07 18:14:00 +0200

    qsfqsf

--- art/image/image.js
+++ art/image/image.js
@@ -5,5 +5,16 @@ const para = document.createElement("img");
 para.setAttribute("src", imageUrl);
 para.setAttribute("id", "pageImage");
 
-var main = document.getElementsByTagName("main")[0];
+var main = document.getElementsByClassName('container')[0];
 main.appendChild(para);
+
+var card = document.createElement('div');
+card.setAttribute('class', 'card');
+
+var cardContent = document.createElement('div');
+cardContent('class', 'cardContent');
+
+const fileName = document.createTextNode(imageUrl.split("/").pop().split(".")[0]);
+cardContent.appendChild(fileName);
+card.appendChild(cardContent);
+main.appendChild(card);
